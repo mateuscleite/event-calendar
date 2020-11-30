fb225f6a421d490df36880e22aebd4af688a458d
mateuscleite <mateus.kastilho@gmail.com>
2020-11-30 15:13:47 -0300

    implemented creation of an event

--- app/src/app/services/events.service.ts
+++ app/src/app/services/events.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { AuthService } from './auth.service';
 import { environment } from 'src/environments/environment';
+import { Event } from '../classes/event';
 import jwt_decode from 'jwt-decode';
 
 @Injectable({
@@ -15,6 +16,11 @@ export class EventsService {
     const token = this.authService.getAuthToken()
     const tokenDecoded = jwt_decode(token)
     return this.http.get<any[]>(`${environment.API}events/owner/${tokenDecoded['id']}`)
+  }
+
+  async newEvent(event: Event){
+    const result = this.http.post<any>(`${environment.API}events`, event).toPromise()
+    return result
   }
 
 }
